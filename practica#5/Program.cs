3a696bd440f55fab3eb72563f2b2b4c1a3974d8f
aidyn <aidynmaskutov2004@gmail.com>
2023-10-26 09:58:53 +0600

    Добавьте файлы проекта.

---
+++ practica#5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Вызываем метод GenerateException, который генерирует исключение
+                HandleException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+
+        static void HandleException()
+        {
+            try
+            {
+                // Генерируем исключение в этом методе
+                throw new ApplicationException("Произошла ошибка в HandleException методе.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Обработка ошибки в HandleException методе: " + ex.Message);
+                // Поднимаем исключение в вызывающий метод
+                throw;
+            }
+        }
+    }
+}
